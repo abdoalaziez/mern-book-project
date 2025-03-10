dedd6c5eeeb0009b5c64ad24d9d9d71bd4b48524
Umran Abdo <Umranabdo112@gmail.com>
2025-03-10 15:44:57 +0200

    update mern-client

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,7 +4,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyD4Dc2cgZEuJRjL_borJ6NN7mQdbnpa6Qk",
   authDomain: "mern-book-inventory-in.firebaseapp.com",
   projectId: "mern-book-inventory-in",
-  storageBucket: "mern-book-inventory-in.appspot.com", // ✅ تم التعديل هنا
+  storageBucket: "mern-book-inventory-in.appspot.com", 
   messagingSenderId: "1053190001889",
   appId: "1:1053190001889:web:6ad841ef87b678f68c53a0"
 };
